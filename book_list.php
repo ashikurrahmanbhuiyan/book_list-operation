addcefc3a031fb8a2333ab0ddad7a412dc20ad4f
ashik <s2010376107@ru.ac.bd>
2023-10-11 16:29:55 +0600

    ekta error tik korchi :)

--- book_list.php
+++ book_list.php
@@ -1,4 +1,5 @@
 <?php
+$josn_file = "book_list.json";
 $arr = json_decode(file_get_contents("book_list.json"), true);
 
 if (isset($_GET['add']) && ($_GET['title'] != '')) {
@@ -33,8 +34,7 @@ if (isset($_GET['search-btn'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>curd operation</title>
     <link rel="stylesheet" href="bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="book_list.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
